8dc5e24dac8a49a27c4b10aaf59b19a246030983
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-14 16:59:45 +0530

    Update ViewAllTasks.jsx

--- front-end/src/Components/ViewAllTasks.jsx
+++ front-end/src/Components/ViewAllTasks.jsx
@@ -10,6 +10,7 @@ const ViewAllTasks = () => {
   useEffect(()=>{
        axios.get('http://localhost:2000/router/viewallTask').then((response)=>{
         setUpcomingTasks(response.data.data);
+
        })
   },[])
 
@@ -17,11 +18,12 @@ const ViewAllTasks = () => {
     navigate(`/editTask/${id}`)
 
   }
+ const theadarray=["Sl.No","Task","Workspace","Due Date","Description","Issued From","Action"]
 
   const done=(id)=>{
     setUpcomingTasks(upcomingTasks.filter((t)=>t._id!==id))
     axios.post(`http://localhost:2000/router/done/${id}`).then((response)=>{
-      console.log("ok");
+      debugger;
     })
 }
   return (
@@ -29,26 +31,27 @@ const ViewAllTasks = () => {
         <div className=' mx-96'>
     <table className='border boredr-slate-200 border-separate border-spacing-2'>
   <thead>
-    <tr><th className='border border-green-900'>Sl.No</th>
-    <th className='border border-green-900'>Task</th>
-    <th className='border border-green-900'>Workspace</th>
-    <th className='border border-green-900'>Due Date</th>
-    <th className='border border-green-900'>Issued From</th>
-    <th className='border border-green-900'>Action</th></tr>
+     <tr>  
+  {theadarray.map((data1,index)=>(
+    
+    <th className='border border-green-900'>{data1}</th>
+   
+   ))}
+   </tr>
   </thead>
   <tbody>
-    {upcomingTasks.map((data,key)=>(<tr>
-    <td className='border border-green-900'>{key+1}</td>
-  <td className='border border-green-900'>{data.task}</td>
-  <td className='border border-green-900'>{data.workspace}</td>
-  <td className='border border-green-900'>{data.date}</td>
-  <td className='border border-green-900'>{data.adddate}</td>
+    {upcomingTasks.map((data,index)=>{ 
+      const mapdata=[index+1,data.task,data.workspace,data.date,data.description,data.adddate]
+    return (<tr>
+     {mapdata.map((data2,index)=>(
+    <td className='border border-green-900'>{data2}</td>
+    ))}
   <td className='border border-green-900'>
     <button class="px-4 py-1 text-sm text-green-900 font-semibold rounded-full border border-green-200 hover:bg-green-600 hover:border-transparent"
     onClick={()=>{done(data._id)}}>Done</button>
     <button class="px-4 py-1 text-sm text-green-900 font-semibold rounded-full border border-green-200 hover:bg-green-600 hover:border-transparent"
-    onClick={()=>{edit(data._id)}}>Edit</button></td></tr>
-  ))}
+    onClick={()=>{edit(data._id)}}>Edit</button></td></tr>)
+    })}
   </tbody></table></div>
   <div>
             <button className=' mt-8 mx-96 px-4 py-1 border font-semibold rounded-full text-sm bg-green-500 bord hover:bg-green-900'>
