d488fbdcb0db5a5ad5b0281474e670e72df0beca
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-18 09:50:21 +0530

    Update Router.js

--- Routers/Router.js
+++ Routers/Router.js
@@ -3,123 +3,73 @@ const taskmodel = require("../Models/Model");
 
 const router = express.Router();
 
-router.post("/addTask", async (req, res) => {
-  try {
-    const { task, date, description, workspace } = req.body;
-    adddate = new Date();
-    taskstatus = "Pending";
-    const result = await taskmodel.create({
-      task,
-      date,
-      description,
-      adddate,
-      workspace,
-      taskstatus,
-    });
-   
+const routeArray = [
+  { method: "post", routes: "/addTask" },
+  { method: "get", routes: "/viewAllTask" },
+  { method: "get", routes: "/viewCompletedTask" },
+  { method: "get", routes: "/editTaskOld/:id" },
+  { method: "post", routes: "/editTask" },
+  { method: "post", routes: "/done/:id" },
+];
 
-    if (result) {
+//Task Body
+const addTask = (req) => {
+  const { task, date, description, workspace } = req.body;
+  adddate = new Date();
+  taskstatus = "Pending";
+  const result = taskmodel.create({
+    task,
+    date,
+    description,
+    adddate,
+    workspace,
+    taskstatus,
+  });
+};
+
+const viewAllTask = () => {
+  const data = taskmodel.find({ taskstatus: "Pending" });
+};
+
+const viewCompletedTask = () => {
+  const data = taskmodel.find({ taskstatus: "Done" });
+};
+
+const editTaskOld = (req) => {
+  const id = req.params.id;
+
+  const oldData = taskmodel.findOne({ _id: id });
+};
+
+const editTask = (req) => {
+  const id = req.body._id;
+  const data = {
+    task: req.body.task,
+    date: req.body.date,
+    description: req.body.description,
+    workspace: req.body.workspace,
+  };
+  const result = taskmodel.updateOne({ _id: id }, { $set: data });
+};
+
+const done = (req) => {
+  const id = req.params.id;
+  const updation = { taskstatus: "Done" };
+  const result = taskmodel.updateOne({ _id: id }, { $set: updation });
+};
+
+//Main Router
+router.post("/addTask", async (req, taskFunction, res) => {
+  await taskFunction(req)
+    .then((respone) => {
       return res.status(200).json({
         success: true,
         error: false,
         data: result,
         messege: "success",
       });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: true, messege: "Error" });
-  }
-});
-
-router.get("/viewallTask", async (req, res) => {
-  try {
-    const data = await taskmodel.find({ taskstatus: "Pending" });
-    if (data) {
-      return res
-        .status(200)
-        .json({ success: true, error: false, data: data, messege: "Success" });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: true, messege: "error" });
-  }
-});
-
-router.get("/completedTask", async (req, res) => {
-  try {
-    const data = await taskmodel.find({ taskstatus: "Done" });
-    if (data) {
-      return res
-        .status(200)
-        .json({ success: true, error: false, data: data, messege: "Success" });
-    }
-    
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: true, messege: "error" });
-  }
-});
-
-router.get("/editTaskOld/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const oldData = await taskmodel.findOne({ _id: id });
-    
-    return res.status(200).json({
-      success: true,
-      error: false,
-      data: oldData,
-      messege: "Success",
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, error: true, messege: "Error" });
     });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: true, messege: "error" });
-  }
 });
-
-router.post("/editTask", async (req, res) => {
-  try {
-    const id = req.body._id;
-    const data = {
-      task: req.body.task,
-      date: req.body.date,
-      description: req.body.description,
-      workspace: req.body.workspace,
-    };
-    const result = await taskmodel.updateOne({ _id: id }, { $set: data });
-    if (result) {
-      return res.status(200).json({
-        success: true,
-        error: false,
-        data: result,
-        messege: "success",
-      });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: true, messege: "error" });
-  }
-});
-router.post("/done/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const updation = { taskstatus: "Done" };
-    const result = await taskmodel
-      .updateOne({ _id: id }, { $set: updation })
-      .then((response) => {
-        return res.status(200).json({
-          success: true,
-          error: false,
-          messege: "Done",
-        });
-      });
-  } catch (error) {}
-});
-module.exports = router;
