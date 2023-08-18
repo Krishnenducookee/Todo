f33a5ab81bfa0263d8e9c7435644188333775ec1
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-18 15:42:02 +0530

    Update Model.js

--- Models/Model.js
+++ Models/Model.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const schema = mongoose.Schema;
+const schema = Schema;
 
-const taskschema = new schema({
-  taskName: { type: String, required: true },
-  dueDate: { type: Date, required: true },
-  isPersonal: { type: Boolean, default: true },
-  taskDescription: { type: String, default: "" },
-  isDone: { type: Boolean, default: false },
-},
-{ timrstamp:true});
+const taskschema = new schema(
+  {
+    taskName: { type: String, required: true },
+    dueDate: { type: Date, required: true },
+    isPersonal: { type: Boolean, default: true },
+    taskDescription: { type: String, default: "" },
+    isDone: { type: Boolean, default: false },
+  },
+  { timestamp: true }
+);
 
-const taskmodel = mongoose.model("task_tb", taskschema);
+const taskmodel = model("task_tb", taskschema);
 
 module.exports = taskmodel;
