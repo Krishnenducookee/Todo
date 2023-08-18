92af337a282768a88bc8870f86fe808c912b79fa
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-18 15:42:47 +0530

    Update Router.js

--- Routers/Router.js
+++ Routers/Router.js
@@ -1,31 +1,69 @@
-const express = require("express");
-const taskBody = require("../Controller/Task_body");
-const router = express.Router();
+const { Router } = require("express");
+const {
+  addTask,
+  viewCompletedTask,
+  viewPendingTasks,
+  done,
+  editTask,
+  getTaskData,
+} = require("../Controller/Task_body");
+
+const router = Router();
 
 const routeArray = [
-  { method: "post", routes: "/addTask" },
-  { method: "get", routes: "/viewAllTask" },
-  { method: "get", routes: "/viewCompletedTask" },
-  { method: "get", routes: "/editTaskOld/:id" },
-  { method: "post", routes: "/editTask" },
-  { method: "post", routes: "/done/:id" },
+  { method: "post", route: "/addTask", routeController: addTask() },
+  {
+    method: "get",
+    route: "/viewAllTask",
+    routeController: viewCompletedTask(),
+  },
+  {
+    method: "get",
+    route: "/viewCompletedTask",
+    routeController: viewPendingTasks(),
+  },
+  {
+    method: "get",
+    route: "/editTaskOld/:id",
+    routeController: getTaskData(),
+  },
+  { method: "post", route: "/editTask", routeController: editTask() },
+  { method: "post", route: "/done/:id", routeController: done() },
 ];
 
 //Main Router
-router.post("/addTask", async (req, taskFunction, res) => {
-  await taskFunction(req)
-    .then((respone) => {
-      return res.status(200).json({
-        success: true,
-        error: false,
-        data: result,
-        messege: "success",
+for (const eachroute in routeArray) {
+  router[eachroute.method](eachroute.route, async (req, res) => {
+    eachroute
+      .routeController(req, res)
+      .then((response) => {
+        res.status(200).json({
+          success: true,
+          error: false,
+          data: response,
+          messege: "success",
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ success: false, error: true, messege: "Error" });
       });
-    })
-    .catch((error) => {
-      res.status(500).json({ success: false, error: true, messege: "Error" });
-    });
-});
+  });
+}
+
+// router.post("/addTask", async (req, taskFunction, res) => {
+//   await taskFunction(req)
+//     .then((respone) => {
+//       return res.status(200).json({
+//         success: true,
+//         error: false,
+//         data: result,
+//         messege: "success",
+//       });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ success: false, error: true, messege: "Error" });
+//     });
+// });
 
 // router.get("/viewallTask", async (req, res) => {
 //   try {
